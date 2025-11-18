a99d5487944d0a2315d6403cd302017ee00d1d2f
Praveen <praveenk@rpsconsulting.in>
2025-11-18 16:02:08 +0530

    added docker file

---
+++ Docker/maths.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number"))
+b = int(input("Enter second number"))
+c = a+b
+d = a-b
+e = a*b
+print(f"The sum is {c}")
+print(f"The difference is {d}")
+print(f"The product is {e}")
